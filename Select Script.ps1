fb96d79cfd3ff06eb606668eef61bd64e3f299cf
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-02 13:56:41 -0800

    Select a Script
    For the use of selecting from Mutliplie on-boarding or off boarding Scripts.

---
+++ Select Script.ps1
@@ -0,0 +1,9 @@
+﻿#Select Script
+Write-Host "*********************************************"
+Write-Host "Which Script would you like to run" -ForegroundColor Yellow
+Write-Host "*********************************************"
+Write-Host ""
+Write-Host ""
+Write-Host "*********************************************"
+$Script = (Read-Host -Prompt "Which Script would you like to run?")
+invoke-expression -Command .\$Script
