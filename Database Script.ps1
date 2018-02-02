f373e5f4967e7b593d0b82b0e763a378edc04377
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-01 16:30:38 -0800

    Rmove test commit
    Remove Test Commit

--- Database Script.ps1
+++ Database Script.ps1
@@ -6,5 +6,3 @@ $linecounter = 1
     }
 Write-host "`r`n"
 $Database = $Databaselist[[int](Read-Host -Prompt "Please enter the number of the Mailbox Database you wish to place this new user in (i.e. 1 or 5)")-1]
-
-#test commit
