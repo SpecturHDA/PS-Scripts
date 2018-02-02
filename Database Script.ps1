6d615051923b31796a49dd491265c2ac4461e8a2
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-01 16:26:11 -0800

    Test Commit
    Testing

--- Database Script.ps1
+++ Database Script.ps1
@@ -1,10 +1,10 @@
 ﻿$Databaselist = Get-MailboxDatabase  | select Name
-
+$linecounter = 1
    foreach($Databasename in $Databaselist){
-      Write-Host($linecounter.ToString() + ". " + $Databasename)
+      Write-Host($linecounter.ToString() + ". " + $Databasename.Name)
       $linecounter++
     }
-    Write-host "`r`n"
-    $Database = $Databaselist[[int](Read-Host -Prompt "Please enter the number of the Mailbox Database you wish to place this new user in (i.e. 1 or 5)")]
-    
-  }
+Write-host "`r`n"
+$Database = $Databaselist[[int](Read-Host -Prompt "Please enter the number of the Mailbox Database you wish to place this new user in (i.e. 1 or 5)")-1]
+
+#test commit
