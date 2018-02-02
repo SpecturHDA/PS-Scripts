4c49e44dc31df998f4b295a1dcd4fdcb92bf0cd7
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-02 13:57:06 -0800

    None command update

--- Create Distrobution Group/Create Distrobution Group v1.ps1
+++ Create Distrobution Group/Create Distrobution Group v1.ps1
@@ -5,7 +5,7 @@ import-module activedirectory
 $Name = (Read-Host -Prompt "Distrobution Group Name")
 $Members = (Read-Host -Prompt "Members email addresses (Seperate each Email address with "," with no spaces.")
 
-#Created Distrobution Group
+#Create Distrobution Group
 Write-Host "************************************************************************************"
 Write-Host "Creating Distrobution Group" -ForegroundColor Yellow
 Write-Host "************************************************************************************"
