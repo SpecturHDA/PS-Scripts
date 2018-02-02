67e2b06b8042a2fcf4e272d60de4374f95d8f3e1
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-02 13:57:35 -0800

    None command update

--- Add Disbrution Group Member/Add Distrobution Group v1.ps1
+++ Add Disbrution Group Member/Add Distrobution Group v1.ps1
@@ -6,7 +6,7 @@ $Name = (Read-Host -Prompt "Distrobution Group Name")
 $Members = (Read-Host -Prompt "Members email addresses (Seperate each Email address with "," with no spaces.")
 
 
-#Created Distrobution Group
+#Add Distrobution Group
 Write-Host "************************************************************************************"
 Write-Host "Creating Distrobution Group" -ForegroundColor Yellow
 Write-Host "************************************************************************************"
