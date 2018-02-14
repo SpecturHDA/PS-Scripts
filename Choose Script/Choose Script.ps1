9638691520424694d7382597e432f767fea6ce5d
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-14 15:45:52 -0800

    Script Code
    Array for Selecting a script to execute

--- Choose Script/Choose Script.ps1
+++ Choose Script/Choose Script.ps1
@@ -1,1 +1,11 @@
-﻿
+Write-Host "1: Script1" -ForegroundColor Yellow
+Write-Host "2: Script2" -ForegroundColor Yellow
+Write-Host "3: Script3" -ForegroundColor Yellow
+$Scripts = @("Script1","Script2","Script3")
+$linecounter = 1
+   foreach($Scriptlist in $Scripts){
+           $linecounter++
+    }
+Write-host "`r`n"
+$Script = $Scripts[[int](Read-Host -Prompt "Please Select a Script to exeicute (i.e. 1,2,or3)")-1]
+invoke-expression -Command .\$Script
