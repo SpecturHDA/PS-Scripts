8c37ed5c46ce795a9911c8c555a7006eb55ad5d0
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-20 16:39:56 -0800

    Script path
    Added Command: Split-Path -Parent $PSCommandPath

--- Choose Script/Choose Script.ps1
+++ Choose Script/Choose Script.ps1
@@ -1,3 +1,5 @@
+Split-Path -Parent $PSCommandPath
+
 Write-Host "1: Script1" -ForegroundColor Yellow
 Write-Host "2: Script2" -ForegroundColor Yellow
 Write-Host "3: Script3" -ForegroundColor Yellow
