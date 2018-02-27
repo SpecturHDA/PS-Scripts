947c08e7b6944105b160f4cf79fee14dbea96224
Chris Morris <35855353+SpecturHDA@users.noreply.github.com>
2018-02-27 08:36:19 -0800

    OU Search
    Code for searching a specific OU and print to an Array

---
+++ OU Search to Array.ps1
@@ -0,0 +1,9 @@
+﻿$OUlist = Get-ADOrganizationalUnit -Filter * -SearchBase "OU=Offices,DC=BNMG,DC=NET" -Properties canonicalname | select canonicalname
+$linecounter = 1
+   foreach($OUname in $OUlist){
+      Write-Host($linecounter.ToString() + ". " + $OUname)
+      $linecounter++
+    }
+Write-host "`r`n"
+$OU = $OUlist[[int](Read-Host -Prompt "Please enter the number of the Mailbox Database you wish to place this new user in (i.e. 1 or 5)")-1]
+$OU
